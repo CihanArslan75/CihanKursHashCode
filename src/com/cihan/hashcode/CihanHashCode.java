94ce254c5cf9a65dbe4d404cbc0a2d8e8f2682e5
cihanarslan75 <cihanarslan75@gmail.com>
2019-04-22 10:35:21 +0300

    HashCode

--- src/com/cihan/hashcode/CihanHashCode.java
+++ src/com/cihan/hashcode/CihanHashCode.java
@@ -8,9 +8,10 @@ import java.util.stream.Collectors;
 public class CihanHashCode {
 	
 	private String result="";
-	private String word = "";
+	private String word1 = "";
 	
 	public String getAsciiCode(String word) {
+		result="";
 		DecimalFormat myFormatter = new DecimalFormat("00000");
 		List<Character> wordList = word.chars().mapToObj(e->(char)e).collect(Collectors.toList()); 
 		wordList.stream().map(d->(int)d*55 ).forEach(d-> {String aa= String.valueOf(myFormatter.format(d));result+=aa;});
@@ -18,9 +19,10 @@ public class CihanHashCode {
 	 }
 	
 	public String decodeAlgrtm(String a) {
+		word1 = "";
 		String[] aa= a.split("(?<=\\G.....)");
 		List<String> aaa=Arrays.asList(aa);
-		aaa.stream().mapToInt(d->Integer.parseInt(d)).forEach(d-> { d=d/55 ;word=word+(char)d ;});
-		return word;	
+		aaa.stream().mapToInt(d->Integer.parseInt(d)).forEach(d-> { d=d/55 ;word1=word1+(char)d ;});
+		return word1;	
 	}	
 }
